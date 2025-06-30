3379210c6131fbbae1e304e2cf3db1d52fd69138
mbene dieye <mbenedieye757@gmail.com>
2025-06-30 11:17:15 +0200

    esercizi

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className='flex-row w-1240 gap-40'>
+        <div className="flex-wrap p-4 boder-black ">
+          <div className="p-8 border 1px w-1/4 " >
+          <img class="mx-auto h-full dark:hidden mb-4" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg" alt=""></img>
+            <div>
+              <h1 className="text-3xl">Apple iMac 27",1TB HDD, Retina 5K Display, M3 Max</h1>
+            </div>
+            <div className="text-3xl font-medium"> $ 1,699</div>
+            <div> <button>Add to cart</button> </div>
+          </div>
+        </div>
+        <div className="w-250">
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
